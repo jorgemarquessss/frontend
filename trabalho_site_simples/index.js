0431f815483842e2c08eb89d2e77034b0ed8b2c4
jorgemarquessss <bigjmarques@gmail.com>
2023-05-08 18:20:13 -0300

    trabalho de layout

--- trabalho_site_simples/index.js
+++
@@ -1,8 +0,0 @@
-function mudarCor() {
-	var botao = document.getElementById("botao");
-	if (botao.classList.contains("verde")) {
-		botao.classList.remove("verde");
-	} else {
-		botao.classList.add("verde");
-	}
-}
